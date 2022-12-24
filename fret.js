b6c4324af662ca29f675b40de9730b274aa8a7de
Styopa Semenukha <semenukha@gmail.com>
2022-12-23 23:25:09 -0500

    Cleanup

--- fret.js
+++ fret.js
@@ -11,17 +11,18 @@ function add_path(element, data, cls) {
   element.appendChild(path);
 }
 
-window.addEventListener('load',
-  function() {
-    const svg = document.getElementById('fretboard');
-    const width = 700, height = 200;
+function draw_fretboard() {
+  const svg = document.getElementById('fretboard');
+  const width = 700, height = 200;
 
-    for (let i = 1; i <= 6; i++) {
-      add_path(svg, `M0 ${height / 7 * i} H ${width}`, 'string');
-    }
+  for (let i = 1; i <= 6; i++) {
+    add_path(svg, `M0 ${height / 7 * i} H ${width}`, 'string');
+  }
 
-    for (let i = 1; i <= 13; i++) {
-      let left = width * (2 - 1.059463 ** (13 - i));
-      add_path(svg, `M${left} 0 V ${height}`, 'fret');
-    }
-  }, false);
+  for (let i = 1; i <= 13; i++) {
+    let left = width * (2 - 1.059463 ** (13 - i));
+    add_path(svg, `M${left} 0 V ${height}`, 'fret');
+  }
+}
+
+window.addEventListener('load', draw_fretboard, false);
