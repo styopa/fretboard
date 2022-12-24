0cc58b8c78e993d3a2177bf7e4d9a5b7ba74feb3
Styopa Semenukha <semenukha@gmail.com>
2022-12-24 13:36:30 -0500

    Draw lines

--- fret.js
+++ fret.js
@@ -1,30 +1,42 @@
 class Fretboard {
   #svg;
+  #x;
+  #y;
   #width;
   #height;
+  #margin;
 
   constructor(svg) {
     this.#svg = svg;
     const dimensions = svg.attributes['viewBox'].value.split(/\D+/);
-    [this.#width, this.#height] = [dimensions[2], dimensions[3]];
+    this.#x = 0;
+    this.#y = 0;
+    this.#margin = 10;
+    this.#width = dimensions[2];
+    this.#height = dimensions[3];
 
     this.add_inlays();
 
     // draw frets
     for (let i = 1; i <= 13; i++) {
-      this.add_child(this.#svg, 'path', {
-        d: `M${this.get_fret_x(i)} 0 V ${this.#height}`,
-        class: 'fret'
-      });
+      this.add_line(true, this.get_fret_x(i), 'fret');
     }
 
     // draw strings
     for (let i = 1; i <= 6; i++) {
-      this.add_child(this.#svg, 'path', {
-        d: `M0 ${this.get_string_y(i)} H ${this.#width}`,
-        class: 'string'
-      });
+      this.add_line(false, this.get_string_y(i), 'string');
     }
+  }
+
+  add_line(vertical, pos, cls) {
+    const params = vertical ?
+      [pos, this.#y, 'V', this.#height] :
+      [this.#x, pos, 'H', this.#width];
+    params.unshift('M');
+    this.add_child(this.#svg, 'path', {
+      d: params.join(' '),
+      class: cls
+    });
   }
 
   get_fret_x(i) {
